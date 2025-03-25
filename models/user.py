fe6fdc7d667cc376fb9585204b8faf5db0f159ae
Joabe Levi <joabe-levi@hotmail.com>
2025-03-24 23:43:00 -0300

    feat: subindo pasta e adicionando metodos ao modelo

---
+++ models/user.py
@@ -0,0 +1,20 @@
+from database import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=True)
+    
+    def __str__(self):
+        return f'{self.username}'
+
+    def save(self, *args, **kwargs):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self, *args, **kwargs):
+        db.session.delete(self)
+        db.session.commit()
